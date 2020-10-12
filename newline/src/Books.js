0d0586adf5da1930ce5c07b8eb24ea3dc5b68e83
Graham Mendick <graham.mendick@gmail.com>
2020-10-13 00:17:36 +0100

    Added paging links

--- newline/src/Books.js
+++ newline/src/Books.js
@@ -1,12 +1,17 @@
 import React, { useEffect, useState } from 'react';
+import { NavigationLink } from 'navigation-react';
 
 function Books({ page = 1 }) {
   const [books, setBooks] = useState([]);
+  const [total, setTotal] = useState(0);
   useEffect(() => {
     fetch(`/api/books?page=${page}`)
       .then(res => res.json())
-      .then(({ books }) => setBooks(books))
-  }, []);
+      .then(({ books, total }) => {
+        setBooks(books);
+        setTotal(total);
+      })
+  }, [ page ]);
   return (
     <>
       <h1>📗 Our Books</h1>
@@ -16,6 +21,17 @@ function Books({ page = 1 }) {
             <img src={book.cover} />
             <h2>{book.title}</h2>
             <div>{book.description}</div>
+          </li>
+        ))}
+      </ul>
+      <ul>
+        {Array(Math.ceil(total / 5)).fill(0).map((_, i) => (
+          <li key={i}>
+            <NavigationLink
+              stateKey="books"
+              navigationData={{ page: i + 1 }}>
+              {i + 1}
+            </NavigationLink>
           </li>
         ))}
       </ul>
