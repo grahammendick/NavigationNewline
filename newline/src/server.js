ae2e445b4c2140ade9a44ed8eb1bf496e72d2f32
Graham Mendick <graham.mendick@gmail.com>
2020-10-12 11:00:33 +0100

    Added book to json api

--- newline/src/server.js
+++ newline/src/server.js
@@ -2,12 +2,17 @@ const express = require('express');
 
 const app = express();
 
+const books = [
+  {
+    slug: 'fullstack-react-with-typeScript',
+    title: 'Fullstack React with TypeScript',
+    cover: 'https://dzxbosgk90qga.cloudfront.net/fit-in/252x329/n/20200515205449509_Book%20Cover-900h.png',
+    description: 'Learn Pro Patterns for Hooks, Testing, Redux, SSR, and GraphQL'
+  }
+]
+
 app.get('/books', (req, res) => {
-  return res.json({
-    x: 1
-  });
+  return res.json(books);
 });
 
-app.listen('3001', () =>
-  console.log('hello'),
-);
+app.listen('3001');
