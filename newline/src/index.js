a5033c0a9c03358ef59ba3d4721ac5e8c8050850
Graham Mendick <graham.mendick@gmail.com>
2020-10-12 09:35:43 +0100

    Rendered welcome view using navigation

--- newline/src/index.js
+++ newline/src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { StateNavigator } from 'navigation';
+import { NavigationHandler } from 'navigation-react';
 import './index.css';
 import App from './App';
+import Welcome from './Welcome';
 import * as serviceWorker from './serviceWorker';
+
+const stateNavigator = new StateNavigator([
+  { key: 'welcome', route: '' },
+  { key: 'books' },
+]);
+
+const { welcome } = stateNavigator.states;
+welcome.renderView = () => <Welcome />;
+
+stateNavigator.start();
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavigationHandler stateNavigator={stateNavigator}>
+      <App />
+    </NavigationHandler>
   </React.StrictMode>,
   document.getElementById('root')
 );
