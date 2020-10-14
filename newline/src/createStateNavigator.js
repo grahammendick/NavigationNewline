77a05ea13a1bb9c8924e972475f9070b32deeb3b
Graham Mendick <graham.mendick@gmail.com>
2020-10-14 10:25:17 +0100

    Created state navigator in separate file

---
+++ newline/src/createStateNavigator.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {HTML5HistoryManager, StateNavigator} from 'navigation';
+import Welcome from './Welcome';
+import Books from './Books';
+import Tutorials from './Tutorials';
+import Book from './Book';
+
+function createStateNavigator() {
+  const stateNavigator = new StateNavigator([
+    {key: 'welcome', route: '', trackTypes: false},
+    {key: 'books', route: 'our-books+/{page}', defaults: {page: 1}, trackTypes: false},
+    {key: 'tutorials', route: 'our-tutorials', defaults: {page: 1}, trackTypes: false},
+    {key: 'book'},
+  ], new HTML5HistoryManager());
+  
+  const {welcome, books, tutorials, book} = stateNavigator.states;
+  welcome.renderView = () => <Welcome />;
+  books.renderView = ({page, title}) => <Books page={page} title={title} />;
+  tutorials.renderView = ({page}) => <Tutorials page={page} />;
+  book.renderView = () => <Book />;
+  return stateNavigator;  
+}
+
+export default createStateNavigator;
--- newline/src/index.js
+++ newline/src/index.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HTML5HistoryManager, StateNavigator} from 'navigation';
 import {NavigationHandler} from 'navigation-react';
 import './index.css';
 import App from './App';
-import Welcome from './Welcome';
-import Books from './Books';
-import Tutorials from './Tutorials';
-import Book from './Book';
+import createStateNavigator from './createStateNavigator';
 import * as serviceWorker from './serviceWorker';
 
-const stateNavigator = new StateNavigator([
-  {key: 'welcome', route: '', trackTypes: false},
-  {key: 'books', route: 'our-books+/{page}', defaults: {page: 1}, trackTypes: false},
-  {key: 'tutorials', route: 'our-tutorials', defaults: {page: 1}, trackTypes: false},
-  {key: 'book'},
-], new HTML5HistoryManager());
-
-const {welcome, books, tutorials, book} = stateNavigator.states;
-welcome.renderView = () => <Welcome />;
-books.renderView = ({page, title}) => <Books page={page} title={title} />;
-tutorials.renderView = ({page}) => <Tutorials page={page} />;
-book.renderView = () => <Book />;
-
+const stateNavigator = createStateNavigator();
 stateNavigator.start();
 
 ReactDOM.render(
