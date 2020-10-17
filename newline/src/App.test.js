79be7356e00d0b2eae72420f7e20420567c469e0
Graham Mendick <graham.mendick@gmail.com>
2020-10-17 18:18:12 +0100

    Tested welcome links navigate to states

--- newline/src/App.test.js
+++ newline/src/App.test.js
@@ -19,3 +19,27 @@ test('welcome renders books and tutorials links', () => {
   assert.strictEqual(links[1].innerHTML, 'tutorials');
   assert.strictEqual(links[1].getAttribute('href'), '/our-tutorials');
 });
+
+test('welcome books link navigates to books', () => {
+  const stateNavigator = createStateNavigator();
+  stateNavigator.navigate('welcome')
+  const {container} = render(
+    <NavigationHandler stateNavigator={stateNavigator}>
+      <App />
+    </NavigationHandler>
+  );
+  container.querySelectorAll('a')[0].click();
+  assert.strictEqual(stateNavigator.stateContext.state.key, 'books');
+});
+
+test('welcome tutorials link navigates to tutorials', () => {
+  const stateNavigator = createStateNavigator();
+  stateNavigator.navigate('welcome')
+  const {container} = render(
+    <NavigationHandler stateNavigator={stateNavigator}>
+      <App />
+    </NavigationHandler>
+  );
+  container.querySelectorAll('a')[1].click();
+  assert.strictEqual(stateNavigator.stateContext.state.key, 'tutorials');
+});
